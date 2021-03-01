02121c5b0e10b412b2fb88be53ca5ceef41aaf87
u000000 <200516@student.glu.nl>
2021-03-01 12:19:35 +0100

    Set up 2

--- webdev-base-webshop-main/core/db_connect.php
+++ webdev-base-webshop-main/core/db_connect.php
@@ -4,18 +4,18 @@ session_start();
 /**
  * Voor de MAC gebruikers;
  */
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "root";
-$dbname = "webshop";
+// $dbhost = "localhost";
+// $dbuser = "root";
+// $dbpass = "root";
+// $dbname = "webshop";
 
 /**
  * Voor de Windows gebruikers;
  */
-// $dbhost = "localhost";
-// $dbuser = "root";
-// $dbpass = "";
-// $dbname = "webshop";
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "webshop";
 
 $con = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
